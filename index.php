e72d1a70f28019f3629c9c56b94eaa6eabab4d4a
Eby-Samuel693 <coquelet.samuel@gmail.com>
2021-02-26 15:24:04 +0100

    juhygvfcd

--- index.php
+++ index.php
@@ -1,19 +1,35 @@
 <?php
 /**
  * 1. Le dossier SQL contient l'export de ma table user.
- * 2. Trouvez comment importer cette table dans une des bases de données que vous avez créées, si vous le souhaitez vous pouvez en créer une nouvelle pour cet exercice.
+ * 2. Trouvez comment importer cette table dans une des bases de données que vous avez
+ *      créées, si vous le souhaitez vous pouvez en créer une nouvelle pour cet exercice.
  * 3. Assurez vous que les données soient bien présentes dans la table.
  * 4. Créez votre objet de connexion à la base de données comme nous l'avons vu
  * 5. Insérez un nouvel utilisateur dans la base de données user
- * 6. Modifiez cet utilisateur directement après avoir envoyé les données ( on imagine que vous vous êtes trompé )
+ * 6. Modifiez cet utilisateur directement après avoir envoyé les données ( on
+ *      imagine que vous vous êtes trompé )
  */
-
+$server = 'localhost';
+$db = 'bdd_cours';
+$user = 'root';
+$pass = '';
 // TODO Votre code ici.
 try {
-    ...
-}
-catch...
 
+    $pdo = new PDO("mysql:host=$server;dbname=$db;charset=utf8mb4", $user,$pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Nous sommes connectés à la base de donnés.";
+
+    $requestU = "
+       INSERT INTO user (nom ,prenom ,rue ,numero ,code_postal ,ville ,pays ,mail)
+       VALUES('Sam','Coquelet','st aubin',6,59330,'limond-fontaine','france','sam.coquelet@gmail.com')
+   ";
+$pdo->exec($requestU);
+
+}
+catch(PDOException $exception) {
+    echo $exception->getMessage();
+}
 
 
 /**
